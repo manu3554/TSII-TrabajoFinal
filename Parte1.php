a68544c0c6474057cd52e26113b25e4a40fc8430
jeantorresa190899 <jeantorresa190899@gmail.com>
2020-01-29 12:43:58 -0500

    12:43

--- Parte1.php
+++ Parte1.php
@@ -1,6 +1,19 @@
 <html>
 
-<head></head>
-<body></body>
+<head>
+<link href="Estilo.css" rel="stylesheet" type="text/css">
+</head>
+<header><img src="img\logo-Essalud.jpg" alt="logo"> </header>
+<body>
+    <nav id="button">
+        <ul>
+            <li>Sitio Principal</li>
+            <li>Acerca de nosotros</li>
+            <li>Registrate</li>
+            <li>Farmacia</li>
+        </ul>
+    </nav>
+
+</body>
 
 </html>
