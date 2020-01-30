d204638886e5d559a6ce7cbc21bfd07f7a5038be
jeantorresa190899 <jeantorresa190899@gmail.com>
2020-01-30 12:56:34 -0500

    menu

--- Index.php
+++ Index.php
@@ -3,16 +3,41 @@
 <head>
 <link href="Estilo.css" rel="stylesheet" type="text/css">
 </head>
-<header><img src="img\logo-Essalud.jpg" alt="logo"> </header>
+
 <body>
-    <nav id="button">
-        <ul>
-            <li>Sitio Principal</li>
-            <li>Acerca de nosotros</li>
-            <li>Registrate</li>
-            <li>Farmacia</li>
-        </ul>
+    <img src="img\logo.jpg" alt="logo">
+    <nav>
+     <ul>
+        <li><a href ="">Farmacia</a>
+            <ul>
+                <li><a href ="">Subtema1.1</a></li>
+                <li><a href ="">Subtema1.2</a></li>
+            </ul>        
+        </li>
+        <li><a href ="">Acerca de nosotros</a>
+            <ul>
+                <li><a href ="">Subtema2.1</a></li>
+                <li><a href ="">Subtema2.2</a></li>
+            </ul>  
+        </li>
+        <li><a href ="">Registrate</a>
+            <ul>
+                <li><a href ="">Subtema3.1</a></li>
+                <li><a href ="">Subtema3.2</a></li>
+            </ul> 
+        </li>
+        <li><a href ="">Sitio Principal</a>
+            <ul>
+                <li><a href ="">Subtema4.1</a></li>
+                <li><a href ="">Subtema4.2</a></li>
+            </ul> 
+        </li>
+    </ul>
     </nav>
+    
+
+
+   
 
 </body>
 
