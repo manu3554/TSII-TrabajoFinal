6225e9cd3991da57e97f130ea149e1e6e6638b00
jeantorresa190899 <jeantorresa190899@gmail.com>
2020-02-16 14:04:05 -0500

    14:04

---
+++ Promociones.php
@@ -0,0 +1,50 @@
+<html>
+    <head><title>Promociones</title>
+    <link href="css/Estilo.css" rel="stylesheet" type="text/css">
+    
+    </head>
+    <body>
+<div id="tituloCitas"><h1>PROMOCIONES DE VERANO</h1></div>
+        <br><br>
+<div class="division" >
+        <table >    
+            <th><a href=""><img src="img/promo1.png" alt="Tips y consejos" class="img-tips"></a></th>
+        </table>
+        <table >
+            <th><a href=""><img src="img/promo2.png" alt="Tips y consejos" class="img-tips"></a></th>
+        </table>
+        <table >          
+            <th><a href=""><img src="img/promo3.png" alt="Tips y consejos" class="img-tips"></a></th>
+        </table>
+</div>
+
+<div class="division" >
+        <table >    
+            <th><a href=""><img src="img/promo4.png" alt="Tips y consejos" class="img-tips"></a></th>
+        </table>
+        <table >
+            <th><a href=""><img src="img/promo5.png" alt="Tips y consejos" class="img-tips"></a></th>
+        </table>
+        <table >          
+            <th><a href=""><img src="img/promo6.png" alt="Tips y consejos" class="img-tips"></a></th>
+        </table>
+</div>
+
+<div class="division" >
+        <table >    
+            <th><a href=""><img src="img/promo7.png" alt="Tips y consejos" class="img-tips"></a></th>
+        </table>
+        <table >
+            <th><a href=""><img src="img/promo8.png" alt="Tips y consejos" class="img-tips"></a></th>
+        </table>
+        <table >          
+            <th><a href=""><img src="img/promo9.jpg" alt="Tips y consejos" class="img-tips"></a></th>
+        </table>
+</div>
+<br><br><br><br>
+
+    <a href="Farmacia.html">COMPRAR</a>
+        <?php
+        ?>
+    </body>
+</html>
---
+++ Tips2.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+        <title>Tips y Consejos</title>
+        <link href="css/Estilo.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div id="tituloTips"><h1>TIPS Y CONSEJOS</h1></div>
+<?php
+    echo ""
+?>    
+</body>
+
+</html>
