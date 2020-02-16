813cbf657c9a3d5663d71dc67ec3185672cb3e06
Manuel Rios <manu3554@gmail.com>
2020-02-15 19:58:51 -0500

    7:58

--- registrate.php
+++ registrate.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         <div id="contenedor"><h1>Registrate</h1></div>
-    <form>
+    <form action="registrate.php" method="post">
         <center>
         <table >
             <tr>
@@ -16,7 +16,7 @@
             <tr>
                 
                 <td>Apellido:<input type="text" name="apellido" placeholder="Ingrese apellido" ></td>
-                <td>Correo:<input type="text" name="correo" placeholder="Ingrese correo" ></td>
+                <td>Usuario:<input type="text" name="usuario" placeholder="Ingrese usuario" ></td>
             </tr>
             <tr>
                 
@@ -25,8 +25,8 @@
             </tr>
             <tr>
                 <td>Sexo:<br/>
-                    <input type="radio" name="generoPersona" placeholder="Hombre">Hombre<br/>
-                    <input type="radio" name="generoPersona" placeholder="Mujer">Mujer
+                    <input type="radio" name="sexo" value="Hombre">Hombre<br/>
+                    <input type="radio" name="sexo" value="Mujer">Mujer
                 </td>
                 <td style="text-align:center"><input id="color" type="submit" value="Registrarse"></td>
                 
@@ -37,7 +37,50 @@
 
     
     <?php
+        if (isset($_POST['nombre'],$_POST['apellido'],$_POST['dni'],$_POST['sexo'],$_POST['telefono'],$_POST['usuario'],$_POST['contra'])) {
+            $dns = "mysql:dbname=farmacia;host=localhost";
+            $user = "root";
+            $pass = "";
+
+            $nombre = $_POST['nombre'];
+            $apellido = $_POST['apellido'];
+            $dni = $_POST['dni'];
+            $sexo = $_POST['sexo'];
+            $telefono = $_POST['telefono'];
+            $usuario = $_POST['usuario'];
+            $contraseña = $_POST['contra'];
+            
+
     
+            try {
+                $con = new PDO($dns,$user,$pass);
+                /*$con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    
+                $stmt=$con->prepare("INSERT INTO clientes VALUES(:nombre, :apellido, :dni, :sexo, :telefono, :usuario
+                , :contraseña)");
+    
+                $stmt->bindParam(':nombre',$nombre);
+                $stmt->bindParam(':apellido',$apellido);
+                $stmt->bindParam(':dni',$dni);
+                $stmt->bindParam(':sexo',$sexo);
+                $stmt->bindParam(':telefono',$telefono);
+                $stmt->bindParam(':usuario',$usuario);
+                $stmt->bindParam(':contraseña',$contraseña);
+    
+                $stmt->execute();*/
+                $sql = "INSERT INTO clientes VALUES('$nombre','$apellido','$dni','$sexo','$telefono','$usuario','$contraseña')";
+                $con->exec($sql);
+                echo "Registrado correctamente";
+    
+    
+            } catch (PDOException $e) {
+                echo "Error:".$e->getMessage();
+            }
+            $con = null; 
+        }else{
+            echo "Rellene los datos";
+        }
+        
 
     ?>
         
