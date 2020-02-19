b24fe9b5f4f60c11bb773e41743ce9cb8f44196f
jeantorresa190899 <jeantorresa190899@gmail.com>
2020-02-18 20:53:58 -0500

    20:53

--- Contactanos.php
+++ Contactanos.php
@@ -1,14 +1,16 @@
 <html>
     <head><title>Contactanos</title>
-         <link href="css/Estilo.css" rel="stylesheet" type="text/css">
+         <link href="css/EstiloTips.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-        <div id="tituloCitas"><h1>CONTACTANOS</h1></div>
+        <div id="tituloTips"><h1>CONTACTANOS</h1></div>
+        <div class="body">
         <br><br>
         <fieldset>
             <legend>&nbsp;&nbsp;HORARIOS DE ATENCION&nbsp;&nbsp;</legend>
             <label>Lunes a Sábado de 7:30 am a 11:00 pm <br>
-                    Domingos y Feriados de 9:00 am a 9:00 pm </label>
+                    Domingos y Feriados de 9:00 am a 9:00 pm <br><br>
+                    *Los horarios de atencion al cliente pueden variar, consulta con nosotros.</label>
         </fieldset>
         <br><br>
         <h1>ENVIANOS UN CORREO</h1>
@@ -26,12 +28,40 @@
                 <input type="submit" name="btnEnviar" value="Enviar correo">
                 <br>
             </fieldset>
-            <br><br>
+            
             <a href="Index.html"><p style="text-align:right">REGRESAR</p></a>
         </form>
+</div>
 
-       
        <?php
+            if(isset($_POST["txtNombre"]))   {
+ 
+                $nombre = $_POST["txtNombre"];
+                $email = $_POST["txtEmail"];
+                $asunto = $_POST["txtAsunto"];
+                $mensaje = $_POST["txtMensaje"];
+        
+                
+                $dns="mysql:dbname=farmacia;host=localhost";
+                $user="root";
+                $pass="";
+        
+                try {
+        
+                $con = new PDO($dns,$user,$pass);
+                $stmt = "INSERT INTO preguntas (nombre,email,asunto,mensaje) VALUES('$nombre','$email','$asunto','$mensaje')";
+        
+                $con->exec($stmt);
+        
+                echo "Se ha enviado su mensaje."."<br>";
+                echo "Trataremos de comunicarnos con usted lo antes posible"."<br>";
+                echo "Gracias.";
+        
+                } catch (PDOException $e) {
+                echo "Error".$e->getMessage();
+                }
+        
+            }
         ?>
     </body>
 </html>
--- Reclamaciones.php
+++ Reclamaciones.php
@@ -9,9 +9,9 @@
     <h1>POR FAVOR, COMPLETE LOS RECUADROS</h1><br>
     <form action="" method="POST">
         <fieldset>
-            <legend> CONSUMIDOR RECLAMANTE </legend>
+            <legend> CONSUMIDOR RECLAMANTE </legend><br>
             <label>Local :</label>
-            <select name="sedes" size ="1" >
+            <select name="local" size ="1" >
                 <option value="Cercado">Cercado</option>
                 <option value="La Molina">La Molina</option>
                 <option value="Santa Anita">Santa Anita</option>
@@ -33,20 +33,54 @@
         </fieldset>
         <br>
         <fieldset>
-            <legend> DATOS DE CONTACTO DEL CLIENTE</legend>
+            <legend> DATOS DE CONTACTO DEL CLIENTE</legend><br>
             <label>Email:</label>
             <input type="text" name="txtEmail"><br><br>
             <label>Tel&eacute;fono :</label>
             <input type="text" name="txtTelefono"><br><br>
             * Es esencial que complete estos recuadros para poder comunicarnos con Ud. <br><br>
+            <label>Mensaje: <br> *Sea breve</label><br>
+            <textarea name="txtMensaje" cols="60" rows="10"></textarea><br><br>
         <input style="padding:7px;" type="submit" name="btnEnviar" value="Enviar"> 
         </fieldset>
     </form>
     <a href="Index.html"><p style="text-align:right">REGRESAR AL MENU PRINCIPAL</p></a> 
 </div>
-        
+    
+<?php
+    if(isset($_POST["txtNombre"]))   {
+
+        $local = $_POST["local"];
+        $nombre = $_POST["txtNombre"];
+        $apellido = $_POST["txtApellido"];
+        $dni = $_POST["txtDni"];
+        $email = $_POST["txtEmail"];
+        $telefono = $_POST["txtTelefono"];
+        $mensaje = $_POST["txtMensaje"];
 
         
+        $dns="mysql:dbname=farmacia;host=localhost";
+        $user="root";
+        $pass="";
+
+        try {
+
+        $con = new PDO($dns,$user,$pass);
+        $stmt = "INSERT INTO reclamos (local,nombre,apellidos,dni,email,telefono,mensaje) VALUES('$local','$nombre','$apellido','$dni','$email','$telefono','$mensaje')";
+
+        $con->exec($stmt);
+
+        echo "Se ha enviado su mensaje de reclamo."."<br>";
+        echo "Trataremos de comunicarnos con usted lo antes posible";
+
+        
+
+        } catch (PDOException $e) {
+        echo "Error".$e->getMessage();
+        }
+
+    }
+?>        
     
        
        
